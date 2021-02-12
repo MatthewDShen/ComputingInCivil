7040688f742fef23dbe6d4f63eb30fd1c2d3c9c7
MatthewDShen <78379726+MatthewDShen@users.noreply.github.com>
2021-02-11 21:38:01 -0500

    Quiz1 Answers

--- Quizzes/Quiz1.py
+++ Quizzes/Quiz1.py
@@ -1,30 +1,37 @@
 # Matthew Shen Quiz 1
 # mds820
 
-scoreList = [
+score_list = [
     'Jackie: 88 94 78 96 100',
     'Mike: 75 78 91 80 84',
     'Jen: 81 72 87 93 95',
     'Chris: 94 70 83 86 90'
 ]
 
-numScores = 5
 
-for i in scoreList:
+for i in score_list:
 
     splitList = i.split(':')
     
     name = splitList[0]
 
-    scores = [splitList[1].split(' ')]
+    scores = splitList[1].split()
 
     floatScores = []
 
-    # for i scores:
-    #     floatScores[i] = float(j)
+    for j in scores:
+        floatScores.append(float(j))
 
-    averageScore = sum(scores) / len(score)
+    averageScore = sum(floatScores) / len(floatScores)
 
     print(name,averageScore)
 
 
+for stu in score_list:
+    name, grades = stu.split(':')
+    grades = [int(i) for i in grades.split()]
+    # grades = map(int(), grades)
+    print(name, sum(grades)/len(grades))
+
+
+#
