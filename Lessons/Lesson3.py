12365b477290456845ba300b7553749115a108fe
MatthewDShen <78379726+MatthewDShen@users.noreply.github.com>
2021-02-18 15:14:41 -0500

    Lesson3.1.py

--- Lessons/Lesson3.py
+++ Lessons/Lesson3.py
@@ -21,20 +21,16 @@ len1 = calc_length((x2 , y2) , (x3 , y3))
 
 len2 = calc_length((x1 , y1) , (x2 , y2))
 
-#print('Length 1:' , len1 , '\nLength2:' , len2)
-
 #Direction Cosines
 
 def dir_cos(xy1,xy2):
     """Returns the direction cosine for a member given points and length"""
 
     len = calc_length(xy1,xy2)
-    return (xy2[0] - xy1[0]/len, xy2[1] - xy1[1]/len)
+    return (xy2[0] - xy1[0])/len, (xy2[1] - xy1[1]/len)
 
 lx1, ly1 = dir_cos((x2,y2), (x3, y3))
 lx2, ly2 = dir_cos((x2, y2), (x1, y1))
-
-print(lx1, ly1, lx2, ly2)
 # Member 1 (btw 2 and 3)
 
 # Member 2 (btw 1 and 2)
@@ -42,18 +38,33 @@ print(lx1, ly1, lx2, ly2)
 # print(lx1, lx2, lx3, lx4)
 
 # Stifness Matrix
-# def stiff_matrix(xy1, xy2):
-#     """Return the stiffness matrix for a member given the """
+def stiff_matrix(xy1, xy2, other_dofs = None):
+    """ Returns the stiffness matrix for a member, given the coordinates of 
+    its near and far end nodes. Can optionally provide a tuple that includes 
+    other degrees of freedom in order to compile a matrix corresponding 
+    to the entire system.
+    """
+    length = calc_length(xy1, xy2)
+    lx, ly = dir_cos(xy1, xy2)
+    k = np.array(
+        [
+        [lx**2, lx*ly, -lx**2, -lx*ly],
+        [lx*ly, ly**2, -lx*ly, -ly**2],
+        [-lx**2, -lx*ly, lx**2, lx*ly],
+        [-lx*ly, -ly**2, lx*ly, ly**2]
+        ]
+    ) / length
     
-#     length = calc_length(xy1, xy2)
-#     lx, ly = dir_cos(xy1, xy2)
+    if other_dofs:
+        for i in other_dofs:
+            k = np.insert(k, i-1, 0, axis=0)
+            k = np.insert(k, i-1, 0, axis=1)
+    
+    return k
 
-#     k1 = np.array(
-#         [lx1**2, lx1*ly1, -lx1**2, -lx1*ly1],
-#         [lx1*ly1, ly1**2, -lx1*ly1, -ly1**2],
-#         [-lx1**2 , -lx1*ly1 , lx1**2 , lx1*ly1]
-#         [-lx1*ly1, -ly1**2, lx1*ly1, ly1**2]
-#     ) / length
+k1 = stiff_matrix((x2, y2), (x3, y3), (5, 6, ))
+k2 = stiff_matrix((x2, y2), (x1, y1) (5, 6, ))
 
+k = k1 + k2
 
-
+print(k)
