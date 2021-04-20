275d88c52f71a64cd8734511dd1e5c55dc135776
MatthewDShen <78379726+MatthewDShen@users.noreply.github.com>
2021-04-20 14:00:34 -0400

    Update Quiz4.py

--- Quizzes/Quiz4.py
+++ Quizzes/Quiz4.py
@@ -29,3 +29,4 @@ class Beam:
         return self.box_area() - (2*(self.height - (2*self.flange_t)) * ((self.flange_w - self.web_t)/2))
 
 w12x35 = Beam(12.50, 0.300, 6.56, 0.52, 10, 35)
+print(w12x35)
